bb2ca25fee587635652a393f50937f69a4c2bdd9
Andreza <andrezasilva744@gmail.com>
2023-03-07 15:48:02 -0300

    Primeiro teste com a verificação do título do site

---
+++ cypress/e2e/home-copy-1.cy.js
@@ -0,0 +1,11 @@
+describe('home', () => {
+  
+	it('aplicação web deve estar online', () => {
+    cy.visit('http://localhost:8081')
+	cy.title().should('eq', 'Gerencie suas tarefas com Mark L')
+ 
+	})
+
+	
+
+})
