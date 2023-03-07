96a4bd56b75c9599267c4a9b879f617b6daf735e
Andreza <andrezasilva744@gmail.com>
2023-03-07 16:51:04 -0300

    XPath dentro do Cypress, ele já dar suporte a isso, não é necessário usar essa extensão

---
+++ cypress/e2e/tasks.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe ('tarefas', () => {
+	it('deve cadastrar uma nova tarefa', () => {
+		cy.visit('http://localhost:8081')
+		//cy.get('#newTask')
+		cy.get('input[placeholder="Add a new Task"]')
+			.type('Ler um livro de node.js')
+		
+		//cy.get('._listButtonNewTask_1y0mp_40') elemento do tipo button, porém ele pode ser alterado dessa forma não será encontrado na próxima refatoração 
+		// button[type="submit"]
+		// button[contains(text(), "Create")] isso abaixo é a mesma coisa que está aqui ao lado da frase
+		cy.contains('button', 'Create').click()
+
+
+	})
+
+})
